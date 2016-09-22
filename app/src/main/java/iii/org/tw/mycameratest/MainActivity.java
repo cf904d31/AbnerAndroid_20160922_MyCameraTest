5508c37cb934d84189baad08bf869dbd05da20fb
cf904d31 <cf904d31@gmail.com>
2016-09-22 16:55:05 +0800

    CameraTest(顯示原圖)沒有成功因為之前亂動到SD卡的檔案位置導致無法取得照片

--- app/src/main/java/iii/org/tw/mycameratest/MainActivity.java
+++ app/src/main/java/iii/org/tw/mycameratest/MainActivity.java
@@ -2,9 +2,13 @@ package iii.org.tw.mycameratest;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -18,11 +22,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        sdroot = Environment.getExternalStorageDirectory();
+
         imgView = (ImageView) findViewById(R.id.imgView);
     }
 
     public void takePicture1(View v) {
         Intent it = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+
+        Uri outputFile = Uri.fromFile(new File(sdroot,"brad.jpg"));
+        it.putExtra(MediaStore.EXTRA_OUTPUT,outputFile);
+
+
         startActivityForResult(it,1);
     }
 
@@ -44,7 +56,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void afterPic2() {
-
+        Bitmap bmp = BitmapFactory.decodeFile(sdroot.getAbsolutePath() + "/brad.jpg");
+        Log.d("Abner",sdroot.getAbsolutePath() + "/brad.jpg");
+        imgView.setImageBitmap(bmp);
     }
 
     public void takePicture2(View v) {
