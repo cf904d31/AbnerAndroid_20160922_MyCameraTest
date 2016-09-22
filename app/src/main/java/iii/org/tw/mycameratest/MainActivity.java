b546d22b947b073c4fd62a90e6e08173e1c73133
cf904d31 <cf904d31@gmail.com>
2016-09-22 14:01:42 +0800

    CameraTest

---
+++ app/src/main/java/iii/org/tw/mycameratest/MainActivity.java
@@ -0,0 +1,22 @@
+package iii.org.tw.mycameratest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView imgView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imgView = (ImageView) findViewById(R.id.imgView);
+    }
+
+    public void takePicture(View v) {
+
+    }
+}
