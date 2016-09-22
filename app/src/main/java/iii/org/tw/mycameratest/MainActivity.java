357dcb2f1cb99d0be018031ba85a17d1228e988a
cf904d31 <cf904d31@gmail.com>
2016-09-22 14:38:23 +0800

    CameraTest(顯示縮圖)

--- app/src/main/java/iii/org/tw/mycameratest/MainActivity.java
+++ app/src/main/java/iii/org/tw/mycameratest/MainActivity.java
@@ -1,13 +1,18 @@
 package iii.org.tw.mycameratest;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
+import java.io.File;
+
 public class MainActivity extends AppCompatActivity {
     private ImageView imgView;
-
+    private File sdroot;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,7 +21,33 @@ public class MainActivity extends AppCompatActivity {
         imgView = (ImageView) findViewById(R.id.imgView);
     }
 
-    public void takePicture(View v) {
+    public void takePicture1(View v) {
+        Intent it = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(it,1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            //afterPic1(data);
+            afterPic2();
+        } else if (requestCode == RESULT_CANCELED) {
+
+        }
+    }
+
+
+    private void afterPic1(Intent data) {
+        Bitmap bmp = (Bitmap) data.getExtras().get("data");
+        imgView.setImageBitmap(bmp);
+    }
+
+    private void afterPic2() {
+
+    }
+
+    public void takePicture2(View v) {
 
     }
 }
